3df4ae2e2afdb19a35cb3ad46dd82ca0992c19fe
RaylILYn <rednoxmc@mail.ru>
2023-03-23 16:19:24 +0300

    Добавьте файлы проекта.

---
+++ ReverseNumber/ReverseNumber/ReverseNumber.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>	
+#include <string>		
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	int chisl;
+	cout << "Введите трехзначное число: ";
+	cin >> chisl;
+	string s = to_string(chisl);
+	reverse(s.begin(), s.end());
+	cout << "Реверс числа: " << s << endl << endl;
+	system("pause");
+}
